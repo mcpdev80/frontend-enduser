5874f68b0fa1a8b8d2a5c832329ed64424088a8d
JosuaKugler <josua.kugler@gmail.com>
2020-03-21 17:32:26 +0100

    different Icon colors implemented (not yet tested)

--- reservation_system_customer/lib/ui/map_page.dart
+++ reservation_system_customer/lib/ui/map_page.dart
@@ -3,17 +3,63 @@ import 'dart:async';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:reservation_system_customer/bloc/bloc.dart';
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
+import 'dart:ui' as ui;
+
+
+enum MarkerColor{
+  green,
+  yellow,
+  red
+}
 
 class MapPage extends StatelessWidget {
+
+  static Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(
+        data.buffer.asUint8List(), targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png)).buffer
+        .asUint8List();
+  }
+
+
+  static Future<BitmapDescriptor> myIconReturningFunction(MarkerColor color) async {
+    switch (color) {
+      case MarkerColor.green:
+        {
+          final Uint8List green =  await getBytesFromAsset('assets/icon_green.png', 70);
+          return BitmapDescriptor.fromBytes(green);
+        }
+        break;
+      case MarkerColor.red:
+        {
+          final Uint8List red =  await getBytesFromAsset('assets/icon_red.png', 70);
+          return BitmapDescriptor.fromBytes(red);
+        }
+        break;
+      case MarkerColor.yellow:
+        {
+          final Uint8List yellow =  await getBytesFromAsset('assets/icon_yellow.png', 70);
+          return BitmapDescriptor.fromBytes(yellow);
+        }
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<MapBloc, MapState>(builder: (context, state) {
       Map<MarkerId, Marker> markers = {};
       if (state is MapLocationsLoaded) {
-        state.locations.forEach((reservation) {
+        state.locations.forEach((reservation) async {
+          final green = await myIconReturningFunction(MarkerColor.green);
           markers[MarkerId(reservation.id)] = Marker(
             markerId: MarkerId(reservation.id),
             position: reservation.position,
+            //icon: green, //retrieve capacity property from reservation
             infoWindow: InfoWindow(
                 title: reservation.name, snippet: "A Short description"),
             onTap: () {},
@@ -43,6 +89,7 @@ class MapViewState extends State<MapView> {
   Completer<GoogleMapController> _controller = Completer();
   final positionTimeout = Duration(seconds: 3);
   LatLng userPosition = LatLng(48.160490, 11.555184); // default position
+
 
   @override
   void initState() {
